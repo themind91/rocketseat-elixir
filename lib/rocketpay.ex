ae2e4801f02a5b49e001233b1da1b215206fb873
Victor Rocha <victorrocha@MacBook-Pro-de-Victor.local>
2021-02-23 20:10:26 -0300

    first commit

---
+++ lib/rocketpay.ex
@@ -0,0 +1,6 @@
+defmodule Rocketpay do
+
+  alias Rocketpay.Users.Create, as: UserCreate
+
+  defdelegate create_user(params), to: UserCreate, as: :call
+end
---
+++ lib/rocketpay_web/views/users_view.ex
@@ -0,0 +1,13 @@
+defmodule RocketpayWb.UsersView do
+  alias Rocketpay.User
+  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+    %{
+      message: "User Created",
+      user: %{
+        id: id,
+        name: name,
+        nickname: nickname
+      }
+    }
+  end
+end
